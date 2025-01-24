f294d973818a0df4716699f6679e2863e11f8ebc
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-01-24 09:01:53 +0800

    Redirecting to Index Added

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+        // Use the following code to redirect to the previous page after creating a record
+        // return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
 }
