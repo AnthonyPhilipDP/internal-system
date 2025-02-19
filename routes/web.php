888641638e6e8a3959ae582f7306ef3e20f1e3f9
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-02-19 14:19:13 +0800

    Pages folder Added

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
 Route::get('/worksheet', function () {
-    return view('worksheet');
+    return view('pages.worksheet');
 });
