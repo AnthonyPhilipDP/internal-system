d2c226550da17928581c7476b2b80f0c87e7a266
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-02-20 11:20:27 +0800

    Italicized some words

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -9,14 +9,14 @@
         <div class="mx-auto max-w-7xl py-32 sm:py-48 lg:py-46">
           <div class="hidden sm:mb-8 sm:flex sm:justify-center">
             <div class="relative rounded-full px-2 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              This is PMSi Internal Website only and is not available online. <a href="https://www.pmsi-cal.com" target="_blank" rel="noopener noreferrer" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Go to Official Website <span aria-hidden="true">&rarr;</span></a>
+              This is PMSi Internal Website only and is not available online. <a href="https://www.pmsi-cal.com" target="_blank" rel="noopener noreferrer" class="font-semibold text-emerald-600"><span class="absolute inset-0" aria-hidden="true"></span>Go to Official Website <span aria-hidden="true">&rarr;</span></a>
             </div>
           </div>
           <div class="text-center">
-            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Precision Measurement Specialists, inc.</h1>
-            <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">A 'metrology' company since 1998</p>
+            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Precision Measurement Specialists, <span class="text-red-500">i</span>nc.</h1>
+            <p class="mt-8 text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">A 'metrology' company since 1998</p>
             <div class="mt-6 flex items-center justify-center gap-x-6">
-              <a href="/admin" class="rounded-full bg-emerald-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500">Administration</a>
+              <a href="/admin" class="rounded-full bg-emerald-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-emerald-500 hover:text-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500">ADMINISTRATION</a>
               {{-- <a href="#" class="rounded-full border border-gray-300 px-6 py-3 text-sm font-semibold text-gray-900 hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Learn more <span aria-hidden="true">→</span></a> --}}
             </div>
             <p class="mt-4 text-center text-gray-700">Welcome to the PMSi internal website. Please click the 'Administration' button to proceed to the admin panel.</p>
