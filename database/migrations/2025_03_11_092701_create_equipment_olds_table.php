3510bee0c89d194158a6a4108d612b0283d5a018
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-03-11 14:11:33 +0800

    Imported the Old Equipment Raw Data Successfully

---
+++ database/migrations/2025_03_11_092701_create_equipment_olds_table.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipment_olds', function (Blueprint $table) {
+            $table->id();
+            $table->string('trans no')->nullable();
+            $table->string('cert no')->nullable();
+            $table->string('date in')->nullable();
+            $table->string('customer id')->nullable();
+            $table->string('po')->nullable();
+            $table->string('realignpo')->nullable();
+            $table->string('repairpo')->nullable();
+            $table->string('pr')->nullable();
+            $table->string('equip id')->nullable();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->string('description')->nullable();
+            $table->string('serial no')->nullable();
+            $table->string('cal date')->nullable();
+            $table->string('cal due')->nullable();
+            $table->string('cod/range')->nullable();
+            $table->string('cal procedure')->nullable();
+            $table->string('prev condition')->nullable();
+            $table->string('condition in')->nullable();
+            $table->string('condition out')->nullable();
+            $table->string('validation')->nullable();
+            $table->string('temp')->nullable();
+            $table->string('humidity')->nullable();
+            $table->string('cal interval')->nullable();
+            $table->string('form no')->nullable();
+            $table->string('ref')->nullable();
+            $table->string('category')->nullable();
+            $table->string('service')->nullable();
+            $table->string('status')->nullable();
+            $table->text('comments')->nullable();
+            $table->string('date out')->nullable();
+            $table->string('visual insp')->nullable();
+            $table->text('accessories')->nullable();
+            $table->string('ack receipt no')->nullable();
+            $table->string('dr_no2')->nullable();
+            $table->string('standards used')->nullable();
+            $table->string('invoiced')->nullable();
+            $table->string('dr_no3')->nullable();
+            $table->string('docdr')->nullable();
+            $table->string('dr_no4')->nullable();
+            $table->string('num_pages')->nullable();
+            $table->string('remarks')->nullable();
+            $table->string('priority remarks')->nullable();
+            $table->string('intermediate check')->nullable();
+            $table->string('caltype')->nullable();
+            $table->string('laboratory')->nullable();
+            $table->string('DocDateReleased')->nullable();
+            $table->string('DrNoDocReleased')->nullable();
+            $table->string('assignedTo')->nullable();
+            $table->string('PersonReceivedDoc')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipment_olds');
+    }
+};
