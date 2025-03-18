2ceeb0022e4e7f4d4ae2b421d6799edc8b0cc84f
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-03-18 13:05:35 +0800

    PMSi Tagline Fixed

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -14,7 +14,8 @@
           </div>
           <div class="text-center">
             <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Precision Measurement Specialists, <span class="text-red-500 italic">i</span>nc.</h1>
-            <p class="mt-8 text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">A 'metrology' company since 1998</p>
+            <p class="mt-8 text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">'a metrology company'</p>
+            <p class="text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">(since 1998)</p>
             <div class="mt-6 flex items-center justify-center gap-x-6">
               <a id="admin-btn" href="/admin" class="rounded-full bg-emerald-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-emerald-500 hover:text-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500">ADMINISTRATION</a>
               {{-- <a href="#" class="rounded-full border border-gray-300 px-6 py-3 text-sm font-semibold text-gray-900 hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Learn more <span aria-hidden="true">→</span></a> --}}
