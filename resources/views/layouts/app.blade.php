4f87ba62cde2cd1b7781428babd661b1e3760020
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-04-08 12:02:38 +0800

    Downgrade tailwind from v4 to v3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet">
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,30 +1,43 @@
 @extends('layouts.app')   
 
 @section('content')
-  <div class="bg-white">
+  <style>
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
+
+    .dynamic-gradient {
+      background: radial-gradient(circle, #d1f1e2, #deebfc, #fceed5, #daf7e1, #f6f8d8);
+      background-size: 300% 300%;
+      animation: gradient 15s linear(0 0%, 0 1.8%, 0.01 3.6%, 0.03 6.35%, 0.07 9.1%, 0.13 11.4%, 0.19 13.4%, 0.27 15%, 0.34 16.1%, 0.54 18.35%, 0.66 20.6%, 0.72 22.4%, 0.77 24.6%, 0.81 27.3%, 0.85 30.4%, 0.88 35.1%, 0.92 40.6%, 0.94 47.2%, 0.96 55%, 0.98 64%, 0.99 74.4%, 1 86.4%, 1 100%) infinite;
+    }
+  </style>
+
+  <div class="dynamic-gradient">
       <div class="relative isolate px-6 pt-2 lg:px-8">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-          <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#8af586] to-[#168507] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-        </div>
         <div class="mx-auto max-w-7xl py-32 sm:py-48 lg:py-46">
           <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div class="relative rounded-full px-2 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div class="relative rounded-full px-2 py-1 text-sm text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               This is PMSi Internal Website only and is not available online. <a href="https://www.pmsi-cal.com" target="_blank" rel="noopener noreferrer" class="font-semibold text-emerald-600"><span class="absolute inset-0" aria-hidden="true"></span>Go to Official Website <span aria-hidden="true">&rarr;</span></a>
             </div>
           </div>
           <div class="text-center">
-            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Precision Measurement Specialists, <span class="text-red-500 italic">i</span>nc.</h1>
-            <p class="mt-8 text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">'a metrology company'</p>
-            <p class="text-lg font-medium italic text-pretty text-gray-500 sm:text-xl/8">(since 1998)</p>
+            <h1 class="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Precision Measurement Specialists, <span class="text-red-500 italic">i</span>nc.</h1>
+            <p class="mt-8 text-lg font-medium italic text-gray-500 sm:text-xl">'a metrology company'</p>
+            <p class="text-lg font-medium italic text-gray-500 sm:text-xl">(since 1998)</p>
             <div class="mt-6 flex items-center justify-center gap-x-6">
               <a id="admin-btn" href="/admin" class="rounded-full bg-emerald-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-emerald-500 hover:text-gray-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500">ADMINISTRATION</a>
-              {{-- <a href="#" class="rounded-full border border-gray-300 px-6 py-3 text-sm font-semibold text-gray-900 hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Learn more <span aria-hidden="true">→</span></a> --}}
             </div>
             <p class="mt-4 text-center text-gray-700">Welcome to the PMSi internal website. Please click the 'Administration' button to proceed to the admin panel.</p>
           </div>
-        </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-          <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#8af586] to-[#168507] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
         </div>
       </div>
   </div>
