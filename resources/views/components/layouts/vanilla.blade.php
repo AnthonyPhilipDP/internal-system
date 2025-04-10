a382da9554f0f5fd412624801a3e2d1f57c9c41b
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-04-10 14:15:38 +0800

    Dynamic Gradient turned to custom external CSS

--- resources/views/components/layouts/vanilla.blade.php
+++ resources/views/components/layouts/vanilla.blade.php
@@ -3,16 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>{{ $title ?? 'PMSi - Internal' }}</title>
-
+    
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
-
+    
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="{{ asset('css/custom/dynamic-gradient.css') }}">
+    
+        <title>{{ $title ?? 'PMSi - Internal' }}</title>
     </head>
     <body>
         {{ $slot }}
