21e5981e08f50ca44cec387ff0ce77c47057b225
AnthonyPhilipDP <anthonyphilipdp+github@gmail.com>
2025-04-14 10:37:48 +0800

    Social Media Links Added

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -98,19 +98,19 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->navigationItems([
                 NavigationItem::make('Facebook')
-                    ->url('https://facebook.com', shouldOpenInNewTab: true)
+                    ->url('https://www.facebook.com/PrecisionMeasurementSpecialists', shouldOpenInNewTab: true)
                     ->icon('heroicon-o-chat-bubble-left-right')
                     ->group('Social Media'),
                 NavigationItem::make('TikTok')
-                    ->url('https://tiktok.com', shouldOpenInNewTab: true)
+                    ->url('https://www.tiktok.com/@pmsi_official', shouldOpenInNewTab: true)
                     ->icon('heroicon-o-chat-bubble-left-right')
                     ->group('Social Media'),
                 NavigationItem::make('Instagram')
-                    ->url('https://instagram.com', shouldOpenInNewTab: true)
+                    ->url('https://www.instagram.com/pmsiofficial/', shouldOpenInNewTab: true)
                     ->icon('heroicon-o-chat-bubble-left-right')
                     ->group('Social Media'),
                 NavigationItem::make('Twitter / X')
-                    ->url('https://x.com', shouldOpenInNewTab: true)
+                    ->url('https://x.com/pmsiofficial', shouldOpenInNewTab: true)
                     ->icon('heroicon-o-chat-bubble-left-right')
                     ->group('Social Media'),
             ])
